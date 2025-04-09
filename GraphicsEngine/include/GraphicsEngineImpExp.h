29e664734308be4a1f43561016196440341003d0
milton <miltonmcdonald1986@gmail.com>
2025-04-09 16:42:37 -0400

    Fixed bug where cross-compatibility failed for linux.

--- GraphicsEngine/include/GraphicsEngineImpExp.h
+++ GraphicsEngine/include/GraphicsEngineImpExp.h
@@ -1,7 +1,13 @@
 #pragma once
 
-#ifdef GRAPHICSENGINE_EXPORTS
-#define GRAPHICSENGINE_API __declspec(dllexport)
+#ifdef _WIN32
+    #ifdef GRAPHICSENGINE_EXPORTS
+		#define GRAPHICSENGINE_API __declspec(dllexport)
+	#else
+		#define GRAPHICSENGINE_API __declspec(dllimport)
+	#endif
+#elif defined(__linux__)
+    #define GRAPHICSENGINE_API
 #else
-#define GRAPHICSENGINE_API __declspec(dllimport)
+    #define GRAPHICSENGINE_API
 #endif
