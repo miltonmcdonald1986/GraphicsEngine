796d1507d29de283b7973eb4d0a4c4cc920cd077
milton <miltonmcdonald1986@gmail.com>
2025-04-11 08:03:22 -0400

    Building "just works" for Windows now, so long as VCPKG_ROOT is defined.

---
+++ BuildAll.ps1
@@ -0,0 +1,12 @@
+﻿$vcpkgDir = $env:VCPKG_ROOT
+
+#Remove build directory
+Remove-Item -Path "build" -Recurse -Force
+
+# Build Debug
+cmake -S . -B build -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE="$vcpkgDir\scripts\buildsystems\vcpkg.cmake"
+cmake --build build --config Debug
+
+# Build Release
+cmake -S . -B build -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE="$vcpkgDir\scripts\buildsystems\vcpkg.cmake"
+cmake --build build --config Release
