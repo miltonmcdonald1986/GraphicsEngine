ddd82fb720b2a0583fd9809dce8feba547690bba
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-04-29 08:15:58 -0400

    Some documentation.

--- GraphicsEngine/include/GraphicsEngine/ICamera.h
+++ GraphicsEngine/include/GraphicsEngine/ICamera.h
@@ -1,3 +1,8 @@
+/// @file ICamera.h
+/// @brief Interface for a camera in the graphics engine.
+/// @author Milton McDonald
+/// @date 2025-04-29
+
 #pragma once
 
 #include "glm/mat4x4.hpp"
@@ -5,11 +10,14 @@
 namespace GraphicsEngine
 {
 
+	/// @brief The ICamera interface represents a camera in the graphics engine.
 	class ICamera
 	{
 	public:
 		virtual ~ICamera() = default;
-
+		
+		/// @brief Gets the projection matrix of the camera.
+		/// @return A 4x4 projection matrix.
 		virtual auto GetViewMatrix() const -> glm::mat4 = 0;
 	};
 
