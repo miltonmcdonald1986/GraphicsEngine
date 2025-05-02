d19b02e332cae0d1207754960a92b3b134007c0e
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-05-02 16:55:19 -0400

    Uniform is very nice now.

--- GraphicsEngine/src/Uniform.h
+++ GraphicsEngine/src/Uniform.h
@@ -1,3 +1,13 @@
+/// @file Uniform.h
+/// @brief Defines the Uniform structure for managing shader uniforms.
+/// 
+/// This file contains the definition of the `Uniform` struct, including
+/// attributes for name, location, and data storage. It is part of the 
+/// graphics engine's shader management system.
+/// 
+/// @author Milton T. McDonald
+/// @date 2025-05-02
+
 #pragma once
 
 #include "Types.h"
@@ -5,13 +15,24 @@
 namespace GraphicsEngine
 {
 
+	/// @brief Represents a shader uniform.
 	struct Uniform
 	{
+		/// @brief Deleted constructor to prevent instantiation.
 		Uniform() = delete;
+
+		/// @brief Constructs a Uniform object with a given name and location.
+		/// @param name The name of the uniform.
+		/// @param location The location of the uniform in the shader program.
 		Uniform(StringView name, int location);
 
+		/// @brief The name of the uniform.
 		const String Name;
+
+		/// @brief The location of the uniform in the shader program.
 		const int Location;
+
+		/// @brief Stores the data associated with this uniform.
 		UniformData Data;
 	};
 
