fa8de34b5ad56aa959abeab6fe35534867df8f66
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-05-21 10:38:19 -0400

    Still working on that build error

--- tests/src/entity_test.cpp
+++ tests/src/entity_test.cpp
@@ -7,41 +7,15 @@
 
 #include <gtest/gtest.h>
 
-#ifdef _WIN32
-#include "EGL/egl.h"
-#else
 #include "GLFW/glfw3.h"
-#endif
+
 #include "GraphicsEngine/IEngine.h"
 
 class GraphicsEngineTestFixture : public ::testing::Test {
  protected:
   void SetUp() override {
-#ifdef _WIN32
-    display_ = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    ASSERT_TRUE(display_);
-    ASSERT_EQ(eglInitialize(display_, nullptr, nullptr), EGL_TRUE);
 
-    EGLConfig config = nullptr;
-    EGLint num_configs;
-    std::vector<EGLint> attribs = {
-        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,  // Request GLES 2.0
-        EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,        // Offscreen rendering
-        EGL_NONE};
-    ASSERT_EQ(
-        eglChooseConfig(display_, attribs.data(), &config, 1, &num_configs),
-        EGL_TRUE);
 
-    std::vector<EGLint> context_attribs = {EGL_CONTEXT_CLIENT_VERSION,
-                                           2,  // Request OpenGL ES 2.0
-                                           EGL_NONE};
-    EGLContext context = eglCreateContext(display_, config, EGL_NO_CONTEXT,
-                                          context_attribs.data());
-    ASSERT_TRUE(context);
-    ASSERT_TRUE(
-        eglMakeCurrent(display_, EGL_NO_SURFACE, EGL_NO_SURFACE, context));
-
-#else
     ASSERT_EQ(glfwInit(), GL_TRUE);
 
     // Create an invisible window for an OpenGL context
@@ -51,35 +25,20 @@ class GraphicsEngineTestFixture : public ::testing::Test {
 
     // Make the context current
     glfwMakeContextCurrent(window_);
-#endif
+
   }
 
   void TearDown() override {
-#ifdef _WIN32
-    eglDestroyContext(display_, context_);
-    eglTerminate(display_);
-#else
     glfwDestroyWindow(window_);
     glfwTerminate();
-#endif
   }
 
  private:
-#ifdef _WIN32
-  EGLContext context_;
-  EGLDisplay display_;
-#else
   GLFWwindow* window_;
-#endif
 };
 
 TEST_F(GraphicsEngineTestFixture, Entity) {
-  #ifdef _WIN32
-  graphics_engine::IEnginePtr engine = graphics_engine::CreateEngine(
-      graphics_engine::GLProcAddressFunc(eglGetProcAddress));
-  #else
   graphics_engine::IEnginePtr engine = graphics_engine::CreateEngine();
-  #endif
   EXPECT_TRUE(engine);
 
   graphics_engine::entities::Entity* entity =
